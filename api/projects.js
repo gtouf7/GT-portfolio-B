4b79ec68033bd6be6ec0506537dae5784b19c255
Georgios Toufexis <georgetouf7@gmail.com>
2024-08-19 17:06:17 -0400

    Update projects.js

--- api/projects.js
+++ api/projects.js
@@ -1,6 +1,11 @@
 const db = require('../modules/db');
 
 module.exports = async (req, res) => {
-    let projectList = await db.getProjects();
-    res.json(projectList);
+    try {
+        let projectList = await db.getProjects();
+        res.status(200).json(projectList);
+    } catch (error) {
+        console.error("Error fetching projects:", error);
+        res.status(500).json({ error: "Internal Server Error" });
+    }
 };
