d9b0b53e1b21c73aeb3c5f18f908e3c8ea0e3608
Georgios Toufexis <georgetouf7@gmail.com>
2024-08-19 17:06:04 -0400

    Update skills.js

--- api/skills.js
+++ api/skills.js
@@ -1,6 +1,11 @@
 const db = require('../modules/db');
 
 module.exports = async (req, res) => {
-    let skillList = await db.getSkills();
-    res.json(skillList);
+    try {
+        let skillList = await db.getSkills();
+        res.status(200).json(skillList);
+    } catch (error) {
+        console.error("Error fetching skills:", error);
+        res.status(500).json({ error: "Internal Server Error" });
+    }
 };
